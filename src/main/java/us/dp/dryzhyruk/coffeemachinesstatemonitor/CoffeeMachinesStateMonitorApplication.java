e69fbf5fce7cf68198c49774470b60ead1cb3958
Ivan Dryzhyruk <ivan.drizhiruk@gmail.com>
2020-10-06 23:08:46 +0300

    Initialisation commit

---
+++ src/main/java/us/dp/dryzhyruk/coffeemachinesstatemonitor/CoffeeMachinesStateMonitorApplication.java
@@ -0,0 +1,13 @@
+package us.dp.dryzhyruk.coffeemachinesstatemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMachinesStateMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMachinesStateMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/dp/dryzhyruk/coffeemachinesstatemonitor/CoffeeMachinesStateMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package us.dp.dryzhyruk.coffeemachinesstatemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMachinesStateMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
